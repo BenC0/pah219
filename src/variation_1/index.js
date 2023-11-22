885289e3fd33b3bc6a8ae4782aebd83856cee8c0
Benji Cohen <suddensquirrel94@gmail.com>
2023-11-22 09:53:14 +0000

    init dev

--- src/variation_1/index.js
+++ src/variation_1/index.js
@@ -5,13 +5,21 @@ import { ExtractVariantName } from "../norman/ExtractVariantName"
 const conditions = _ => {
     let conditions = [
         !!document.querySelector(`body`),
+        !!document.querySelector(`.AMP-ribbon-banner__item [href^="/shop/en/pets/delivery"]`),
     ]
     return conditions.every(a => a)
 }
 
 function action() {
     this.log("Action loaded")
+    let desktop_image = `https://editor-assets.abtasty.com/47297/655dce5b55bcf1700646491.jpg`
+    let mobile_image = `https://editor-assets.abtasty.com/47297/655dce74e46641700646516.jpg`
     console.warn(this)
+    let target = document.querySelector(`.AMP-ribbon-banner__item [href^="/shop/en/pets/delivery"]`)
+    target.innerHTML = `<picture>
+        <source srcset="${desktop_image}" media="(min-width: 650px)" />
+        <img src="${mobile_image}" alt="Free home delivery in 4 working days whenm you spend £45+" />
+    </picture>`
 }
 
 function fallback() {
